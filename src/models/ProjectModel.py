7120ea743593972ffd41584c4379a59e512f08e9
Adnan El Assadi <aassadi22@ku.edu.tr>
2025-07-02 03:30:52 -0400

    Indexing + Asset

--- src/models/ProjectModel.py
+++ src/models/ProjectModel.py
@@ -1,5 +1,5 @@
 from .BaseDataModel import BaseDataModel
-from .db_schemas.project import Project
+from .db_schemas import Project
 from .enums.DataBaseEnum import DataBaseEnum
 
 class ProjectModel(BaseDataModel):
@@ -29,7 +29,7 @@ class ProjectModel(BaseDataModel):
     async def create_project(self, project: Project):
 
         result = await self.collection.insert_one(project.model_dump(by_alias=True, exclude_unset=True))
-        project._id = result.inserted_id
+        project.id = result.inserted_id
 
         return project
     
